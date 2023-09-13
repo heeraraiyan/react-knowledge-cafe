5cdc53d7c7a2b6ef691408bdb413ffab1662d2c9
heeraraiyan <heeraraiyan07@yahoo.com>
2023-09-13 19:12:24 +0600

    blog section design done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -3,10 +3,30 @@ import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
     // console.log(blog)
-    const {title} = blog;
+    const {title,cover,author_img,reading,author,posted,hashtags} = blog;
     return (
         <div>
+            
+            <img className='w-full mt-20' src={cover} alt="" />
+            <div className='flex justify-between mt-2'>
+                <div className='flex'>
+                <img className='w-14' src={author_img} alt="" />
+                <div className='ml-2'>
+                    <h5>{author}</h5>
+                    <p>{posted}</p>
+                </div>
+                </div>
+                <div>
+                    <span>{reading} min read</span>
+                </div>
+            </div>
+
             <h2 className='text-4xl'>{title}</h2>
+            <div className='mt-4 mb-10'>
+                {
+                    hashtags.map((hash,idx)=> <span key={idx}><a href=''>#{hash}</a></span>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -2,7 +2,7 @@
 
 const Bookmarks = () => {
     return (
-        <div className="md:w-1/3">
+        <div className="md:w-1/3 text-center">
             <h2 className="text-4xl">Bookmarks</h2>
             
         </div>
